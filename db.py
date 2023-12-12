22fbab4ee4479da377cae35e05b1c2786b7b6802
maximesunnen <maximesunnen@gmail.com>
2023-12-12 09:14:27 +0100

    load_db, add_item, db_to_table, find_item functions

--- db.py
+++ db.py
@@ -1,28 +1,102 @@
 import sqlite3
 from sqlite3 import Error
+from tabulate import tabulate
+import re
+import sys
 
-def connect_db(db_file):
+def connect_db(database_file):
     """ Create a database connection to a database that resides
         in the memory; initialize database using schema.sql file
     """
-    connection = None
+    con = None
     
     # Connect to database
     try:
-        connection = sqlite3.connect(db_file)
-        print(sqlite3.version)
-        return connection
+        con = sqlite3.connect(database_file)
+        return con
     except Error as e:
         print(e)
     
-    return connection
+    return con
     
-def init_db(connection):
+def init_db(con):
     """
     Initialize database. Function with no return value but side effect.
     """
     # Create a new cursor object
-    db = connection.cursor()
+    cur = con.cursor()
     
     with open("schema.sql") as f:
-        db.executescript(f.read())
+        cur.executescript(f.read())
+        
+    con.close()
+    
+def load_db(database_file):
+    # connect to database; change row_factory
+    con = connect_db("inv.db")
+    con.row_factory = sqlite3.Row
+    
+    # create a database cursor
+    cur = con.cursor()
+    
+    # fetch data and create dict
+    cur.execute("SELECT * FROM inv")
+    dict = {}
+
+    for r in cur.fetchall():
+        dict.update({r["item"]: r["quantity"]})
+        
+    # close connection
+    con.close()
+    
+    return dict
+
+def add_item(cur, inv):
+    try:
+        item = input("Item to add: ").upper().strip()
+        quantity = input("Quantity: ").strip()
+    except EOFError:
+        print("\nExiting program...")
+        return True
+    
+    # convert quantity to int
+    try:
+        quantity = int(quantity)
+    except ValueError:
+        print("Invalid quantity")
+        return False
+
+    if item in inv.keys():
+        # get current quantity
+        new_quantity = inv[item] + int(quantity)
+        
+        cur.execute("UPDATE inv SET quantity = ? WHERE item = ?", (new_quantity, item))
+        
+    else:
+        cur.execute("INSERT INTO inv (item, quantity) VALUES (?, ?)", (item, quantity))
+        
+    return False
+
+def db_to_table(DB_FILE_NAME):
+    # show inventory as table
+    inv = load_db(DB_FILE_NAME)
+    
+    inv_list = []
+    
+    for key in inv.keys():
+        inv_list.append([key, inv[key]])
+        
+    return tabulate(inv_list, tablefmt="grid")
+
+def find_item(inv):
+    try:            
+        item = input("Search item: ").upper().strip()
+    except EOFError:
+        print("\nExiting search.")
+        sys.exit(0)
+    
+    pattern = re.compile(f"^.*{item}.*$")
+
+    matching_data = [[key, value] for key, value in inv.items() if pattern.match(key)]
+    
+    return item, matching_data
