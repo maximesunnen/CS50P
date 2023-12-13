dea1e1158f042c4fc7e191fde432cd4d39796432
maximesunnen <maximesunnen@gmail.com>
2023-12-13 13:12:39 +0100

    Added UNIQUE constraint to item

--- schema.sql
+++ schema.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS inv;
 -- Table to store user (child) information
 CREATE TABLE inv (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  item TEXT NOT NULL,
+  item TEXT NOT NULL UNIQUE,
   quantity INTEGER NOT NULL
 );
