2a01540ab1e1b4d5a6691529388d8860ecf41ffd
maximesunnen <maximesunnen@gmail.com>
2023-12-13 13:13:39 +0100

    Restructured code for readability and conciseness

--- db.py
+++ db.py
@@ -1,5 +1,4 @@
 import sqlite3
-from sqlite3 import Error
 from helpers import get_item_input, get_quantity_input
 from tabulate import tabulate
 import re
@@ -7,22 +6,23 @@ import sys
 
 def connect_db(DB_FILE_NAME):
     """
-    Create a database connection to a database that resides in the memory
+    Open a connection to a SQLite database. If a sqlite3.Error is raised, exits the program.
+    :param DB_FILE_NAME: The path to the database file to be opened
+    :type DB_FILE_NAME: string
     """
     con = None
     
-    # connect to database
     try:
         db = sqlite3.connect(DB_FILE_NAME)
         return db
-    except Error as e:
-        print(e)
-    
-    return sqlite3.connect(DB_FILE_NAME)
+    except sqlite3.Error as e:
+        sys.exit(f"Error connecting to the database: {e}")
     
 def init_db(DB_FILE_NAME, SCHEMA_SQL):
     """
-    Connect to and initialize database. Side effect function.
+    Initialize the database.
+    :param str DB_FILE_NAME: Path to the database file to be opened
+    :param str SCHEMA_SQL: Path to the schema.sql file to be used for initialization
     """
     with connect_db(DB_FILE_NAME) as db:
         cur = db.cursor()
@@ -32,14 +32,15 @@ def init_db(DB_FILE_NAME, SCHEMA_SQL):
     
 def load_db(DB_FILE_NAME):
     """
-    Returns the database as a dict object with item-quantity pairs.
+    Load the database into memory as a dict object.
+    :param str DB_FILE_NAME: Path to the database file to be loaded
     """
     with connect_db(DB_FILE_NAME) as db:
-        # change row_factory, create cursor
+        # change row_factory
         db.row_factory = sqlite3.Row
         cur = db.cursor()
     
-        # fetch db data
+        # fetch data
         cur.execute("SELECT * FROM inv")
         
         dict = {}
@@ -52,6 +53,9 @@ def load_db(DB_FILE_NAME):
 def add_item(inv, db, cur):
     """
     Add item to the database. Return True if the user pressed CTRL+D, False otherwise.
+    :param dict inv: Dict object representing the database.
+    :param sqlite3.Connection db: A SQLite database connection.
+    :param sqlite3.Cursor cur: A SQLite Cursor instance.
     """
     
     # Get item
@@ -60,41 +64,36 @@ def add_item(inv, db, cur):
     if item is None:
         return True
     
-    # Get quantity, convert to int
+    # Get quantity and validate
     quantity = get_quantity_input("Quantity")
     
-    if quantity is not None:
-        try:
-            quantity = int(quantity)
-        except ValueError:
-            print("Invalid quantity")
-            return False
-    else:
+    if quantity is None:
         return True
+        
+    try:
+        quantity = int(quantity)
+    except ValueError:
+        print("Invalid quantity")
+        return False
     
     # Validate inputs and add items
     if item in inv:
         inv[item] += quantity
-        
-        try:
-            cur.execute("UPDATE inv SET quantity = ? WHERE item = ?", (inv.get(item), item))
-        except db.IntegrityError:
-            print("Error 003: db.IntegrityError")
-
     else:
-        try:
-            inv.update({item: quantity})
-            cur.execute("INSERT INTO inv (item, quantity) VALUES (?, ?)", (item, quantity))
-        except db.IntegrityError:
-            print("Error 003: db.IntegrityError")
-        
+        inv.update({item: quantity})
+
+    try:
+        cur.execute("INSERT INTO inv (item, quantity) VALUES (?, ?) ON CONFLICT(item) DO UPDATE SET quantity = ?", (item, inv.get(item), inv.get(item)))
+    except db.IntegrityError:
+        print("Error 003: db.IntegrityError")
+
     return False
 
 def remove_item(inv, db, cur):
     """
-    Get and verify inputs and remove them from the database. Returns True to signal program exit, False otherwise.
+    Get item and quantity, validate and remove item from the database. Returns True to signal program exit, False otherwise.
     """
-    # item input
+    # Get item
     item = get_item_input("Item to remove")
     
     if item is None:
@@ -104,20 +103,18 @@ def remove_item(inv, db, cur):
         print("Item not in inventory")
         return False
     
-    # quantity input
+    # Get quantity and validate
     quantity = get_quantity_input("Quantity")
     
     if quantity is None:
         return True
     
     try:
-        quantity = int(quantity)
+        if (quantity := int(quantity)) > inv.get(item, 0):
+            print("Removing more items than you own")
+            return False
     except ValueError:
-        print("Quantity is not an integer")
-        return False
-
-    if quantity > inv.get(item, 0):
-        print("Removing more items than you own")
+        print("Invalid quantity")
         return False
     
     # database logic
@@ -131,12 +128,15 @@ def remove_item(inv, db, cur):
     return False
 
 def find_item(inv):
+    """
+    Find item in inventory using regular expression matching. Return list of matching items.
+    """
     item = get_item_input("Search item")
     
     if item is None:
         sys.exit(0)
     
-    # regular expression
+    # Regular expression
     pattern = re.compile(f"^.*{item}.*$")
 
     # find matches
