7b34213e987221a3ecdf034e40698411f57249db
maximesunnen <maximesunnen@gmail.com>
2023-12-19 16:39:01 +0100

    Unit testing for find_item and connect_db

--- test_project.py
+++ test_project.py
@@ -2,9 +2,11 @@ from project import add_item, remove_item, find_item
 from db import load_db
 from db import connect_db
 import pytest
+import sqlite3
 
 SCHEMA_SQL = "schema.sql"
-DB_FILE_NAME = "test_inv.db"
+DB_FILE_NAME = "inv.db"
+TEST_DB_FILE_NAME = "test_inv.db"
 
 TEST_ITEM_1 = "MILK"
 TEST_ITEM_2 = "CAFÉ"
@@ -21,23 +23,31 @@ class TestClass:
         Initialize the testing db.
         """
 
-        with connect_db(DB_FILE_NAME) as db:
+        with connect_db(TEST_DB_FILE_NAME) as db:
             cur = db.cursor()
         
             with open(SCHEMA_SQL) as f:
                 cur.executescript(f.read())
             
             print(("Test db initialized."))
+            
+    def test_connect_db(self):
+        """
+        Test connection to the main database.
+        """
+        db = connect_db(DB_FILE_NAME)
+        
+        assert isinstance(db, sqlite3.Connection)
 
     def test_add_item(self):
         # Initialize db
         self.init_db()
                 
         # Load the db as dict
-        inv = load_db(DB_FILE_NAME)
+        inv = load_db(TEST_DB_FILE_NAME)
         
         # Connect to db
-        with connect_db(DB_FILE_NAME) as db:
+        with connect_db(TEST_DB_FILE_NAME) as db:
             cur = db.cursor()
             
             # Add item to the db
@@ -56,18 +66,18 @@ class TestClass:
         self.init_db()
         
         # Load the db as dict
-        inv = load_db(DB_FILE_NAME)
+        inv = load_db(TEST_DB_FILE_NAME)
         
         # Connect to db
-        with connect_db(DB_FILE_NAME) as db:
+        with connect_db(TEST_DB_FILE_NAME) as db:
             cur = db.cursor()
             
-            # Add test item to the db
+            # Add test items to the db
             add_item(TEST_ITEM_1, TEST_QUANTITY_1, inv, db, cur)
             add_item(TEST_ITEM_2, TEST_QUANTITY_2, inv, db, cur)
             add_item(TEST_ITEM_3, TEST_QUANTITY_3, inv, db, cur)
 
-            # Remove test item from the db
+            # Remove test items from the db
             remove_item(TEST_ITEM_1, 1, inv, db, cur)
             remove_item(TEST_ITEM_2, 1, inv, db, cur)
             remove_item(TEST_ITEM_3, 1, inv, db, cur)
@@ -78,5 +88,32 @@ class TestClass:
         assert quantity_1 == TEST_QUANTITY_1 - 1
         assert quantity_2 == TEST_QUANTITY_2 - 1
         
+        # Test that items with quantity = 0 are completely removed from the db
         with pytest.raises(TypeError):
             cur.execute("SELECT quantity FROM inv WHERE item = ?", (TEST_ITEM_3,)).fetchone()[0]
+            
+    def test_find_item(self):
+        # Initialize db
+        self.init_db()
+        
+        # Load the db as dict
+        inv = load_db(TEST_DB_FILE_NAME)
+        print(inv)
+        
+        # Add test item to the db
+        with connect_db(TEST_DB_FILE_NAME) as db:
+            db.row_factory = sqlite3.Row
+            cur = db.cursor()
+            add_item(TEST_ITEM_1, TEST_QUANTITY_1, inv, db, cur)
+            cur.execute("SELECT * FROM inv")
+        
+        # Load the db as dict
+        inv = load_db(TEST_DB_FILE_NAME)
+        print(inv)
+        
+        # Find item
+        item_1, matching_data = find_item(TEST_ITEM_1, inv)
+        assert matching_data == [[TEST_ITEM_1, TEST_QUANTITY_1]]
+        
+        item_2, matching_data = find_item(TEST_ITEM_2, inv)
+        assert matching_data == []
